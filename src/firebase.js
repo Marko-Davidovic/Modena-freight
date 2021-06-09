693bc73b4a40021e6d9ea7532d5520697b44215a
Marko Davidovic <davidovic-m@hotmail.com>
2021-06-09 14:42:44 -0400

    update login

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,13 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
+
+	//apiKey: "AIzaSyBm0T-uZU26YGgUdq1nbtXcRuxCmAM3084",
+	//authDomain: "modena-freight-789cb.firebaseapp.com",
+	//projectId: "modena-freight-789cb",
+	//storageBucket: "modena-freight-789cb.appspot.com",
+	//messagingSenderId: "333264384577",
+	//appId: "1:333264384577:web:d3fc60b9b4921474df4cc0"
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PRODJECT_ID,
